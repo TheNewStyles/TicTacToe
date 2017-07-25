8878f8d196cc2c7119093fe5df491b434bc6cc96
TheNewStyles <tyler.peterson@utah.edu>
2017-07-24 18:51:55 -0600

    Populate blank board arrray

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+(function() {
+
+  var board = [];
+
+  (function(){
+    for(var i=0; i<=2; i++){
+        board.push([0,0,0]);
+    }
+  })();
+
+  console.log(board);
+  // Your custom JavaScript goes here
+})();
