7a815f78681250f2fde3411fdf92d9bede03d04b
TheNewStyles <tyler.peterson@utah.edu>
2017-07-25 20:36:05 -0600

    Add basic JS for placing rotating X's and O's into grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,29 @@
 (function() {
 
-  var board = [];
+    var board = [];
+    var mainClass = document.getElementsByClassName("main");
+    var counter = 0;
 
-  (function(){
-    for(var i=0; i<=2; i++){
+    (function(){
+    for (var i = 0; i <= 2; i++){
         board.push([0,0,0]);
     }
-  })();
+    })();
+    console.log(board);
 
-  console.log(board);
-  // Your custom JavaScript goes here
+    function cool() {
+        counter++;
+
+        if (counter % 2 === 0){
+            this.innerText = "X";
+        }
+        else {
+            this.innerText = "O";
+        }
+    }
+
+    Array.from(mainClass).forEach(function(element) {
+        element.addEventListener('click', cool);
+    });
+
 })();
