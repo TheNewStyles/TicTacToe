1e4ef9830b3c07a28a576e3b7f7d0aa6c4aae3fd
TheNewStyles <tyler.peterson@utah.edu>
2017-07-26 18:56:18 -0600

    Add row and column win check TODO implement diag win check

--- scripts/main.js
+++ scripts/main.js
@@ -3,27 +3,94 @@
     var board = [];
     var mainClass = document.getElementsByClassName("main");
     var counter = 0;
+    var playerX = "X";
+    var playerO = "O";
 
-    (function(){
-    for (var i = 0; i <= 2; i++){
-        board.push([0,0,0]);
-    }
-    })();
-    console.log(board);
+    initBoard();
 
-    function cool() {
-        counter++;
-
-        if (counter % 2 === 0){
-            this.innerText = "X";
-        }
-        else {
-            this.innerText = "O";
-        }
-    }
 
     Array.from(mainClass).forEach(function(element) {
-        element.addEventListener('click', cool);
+            element.addEventListener('click', start);
     });
 
+    function initBoard() {
+        for (var i = 0; i <= 2; i++){
+            board.push([0,0,0]);
+        }
+    }
+
+    function start() {
+        if(this.innerText != playerX && this.innerText != playerO) {
+            counter++;
+
+            if (counter % 2 === 0) {
+                this.innerText = playerX;
+                executePlayersTurn(this.id, playerX)
+                checkForWinnerHorizontal();
+                checkForWinnerVertically();
+            }
+            else {
+                this.innerText = playerO;
+                executePlayersTurn(this.id, playerO);
+                checkForWinnerHorizontal();
+                checkForWinnerVertically();
+            }
+        }
+    }
+
+    function executePlayersTurn(id, player) {
+        var row = id.slice(0,1);
+        var col = id.slice(2,3);
+
+        board[row][col] = player;
+        console.log(board);
+    }
+
+    function checkForWinnerHorizontal() {
+        for (var i = 0; i < board.length; i++){
+            var oCounter = 0;
+            var xCounter = 0;
+            for (var j = 0; j < board[i].length; j++){
+                if (board[i][j] === playerO) {
+                    oCounter++;
+                    if(oCounter > 2){
+                        console.log("O PLAYER WON");
+                    }
+                }else if (board[i][j] === playerX){
+                    xCounter++;
+                    if(xCounter > 2){
+                        console.log("X PLAYER WON");
+                    }
+                }
+            }
+        }
+    }
+
+    function checkForWinnerVertically() {
+
+        for (var i = 0; i < board.length; i++){
+            iterateThroughColumn(i);
+        }
+    }
+
+    function iterateThroughColumn(column) {
+        var oCounter = 0;
+        var xCounter = 0;
+
+        for (var i = 0; i < board.length; i++)
+        {
+            if (board[i][column] === playerO){
+                oCounter++;
+                if (oCounter > 2){
+                    console.log("O PLAYER WON");
+                }
+            }
+            if (board[i][column] === playerX){
+                xCounter++;
+                if (xCounter > 2){
+                    console.log("X PLAYER WON");
+                }
+            }
+        }
+    }
 })();
